2fbdea70180b49f54b8620241beca5392e472b92
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-02-24 23:35:25 +0300

    Project Setup

---
+++ src/main/java/net/snowyquest/rpgmine/RPGMinePlugin.java
@@ -0,0 +1,17 @@
+package net.snowyquest.rpgmine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGMinePlugin extends JavaPlugin { 
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
